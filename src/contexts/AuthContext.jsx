5652c2896dd083513dd2a3a5252cae072b925628
Carlos Eduardo <eduardosantoos573@gmail.com>
2024-01-11 22:07:03 -0300

    update: name router and api backend

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,82 +1,83 @@
 import { createContext, useEffect, useState } from 'react'
 import {setCookie, parseCookies} from 'nookies'
-import Router from 'next/router'
+import router from 'next/router'
 import {api} from '../services/api'
 
 export const AuthContext = createContext({})
 
 export function AuthProvider({children}) {
-  const [user, setUser] = useState({})
+	const [user, setUser] = useState({})
  
-  const isAuthenticated = !!user
+	const isAuthenticated = !!user
 
-  useEffect(() => {
-    const {'authcookie': token} = parseCookies()
+	useEffect(() => {
+		const {'authcookie': token} = parseCookies()
     
-    async function handleUser(token) {
-      if(token){
-        try {
-          const response = await api().get('/profile')
-          const {data: user} = response
+		async function handleUser(token) {
+			if(token){
+				try {
+					const response = await api().get('/user')
+					const {data: user} = response
           
-          setUser(user)
-        } catch (err) {
-          console.error(err.message)
-        }
-      }
-    }
+					setUser(user)
+				} catch (err) {
+					console.error(err.message)
+				}
+			}
+		}
 
-    handleUser(token)
-  }, [])
+		handleUser(token)
+	}, [])
 
-  async function signIn({email, password}){
-    try {
-      const response = await api().post('/auth/login/', {
-        email,
-        password
-      })
+	async function signIn({email, password}){
+		try {
+			const response = await api().post('/auth/login/', {
+				email,
+				password
+			})
 
-      const { token, user } = response.data
+			const { token, user } = response.data
 
-      setCookie(undefined, 'authcookie', token, {
-        maxAge: 60 * 60 * 24, // 1 dia expiração
-        sameSite: 'strict',
-        secure: false
-      })
+			setCookie(undefined, 'authcookie', token, {
+				maxAge: 60 * 60 * 24, // 1 dia expiração
+				sameSite: 'strict',
+				secure: true
+			})
 
-      setCookie(undefined, 'authcookie.role', user.role, {
-        maxAge: 60 * 60 * 24, // 1 dia expiração
-        sameSite: 'lax',
-        secure: false
-      })
+			setCookie(undefined, 'authcookie.role', user.role, {
+				maxAge: 60 * 60 * 24, // 1 dia expiração
+				sameSite: 'strict',
+				secure: true,
+				// httpOnly: true
+			})
 
-      api().defaults.headers['Authorization'] = `Bearer ${token}`
+			api().defaults.headers['Authorization'] = `Bearer ${token}`
 
-      setUser(user)
+			setUser(user)
       
-      Router.push('/changelog')
-    } catch (err) {
-      console.error(err.message)
-    }
-  }
+			router.push('/changelog')
+		} catch (err) {
+			console.error(err.message)
+		}
+	}
 
-  async function changePassword({email, new_password, confirm_password}){
-    try {
-      await api().put('/forgetpassword', {
-        email,
-        new_password,
-        confirm_password
-      })
+	async function changePassword({email, new_password, confirm_password}){
+		try {
+			await api().put('/forgetpassword', {
+				email,
+				new_password,
+				confirm_password
+			})
 
-      Router.push('/signIn/auth')
-    } catch (err) {
-      console.error(err.message)
-    }
-  }
+			router.push('/signIn/auth')
+		} catch (err) {
+			console.error(err.message)
+		}
+	}
 
-  return (
-    <AuthContext.Provider value={{user, isAuthenticated, signIn, changePassword}}>
-      {children}
-    </AuthContext.Provider>
-  )
+	return (
+		<AuthContext.Provider value={{user, isAuthenticated, signIn, changePassword}}>
+			{children}
+		</AuthContext.Provider>
+	)
 }
