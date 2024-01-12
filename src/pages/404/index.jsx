e8518ec2733f8a7ba6e9eff8aa3800f83341b1b2
Carlos Eduardo <eduardosantoos573@gmail.com>
2024-01-11 22:12:03 -0300

    add: styles on page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,6 +1,9 @@
+import styles from '@/pages/404/styles.module.css'
 export default function Custom404() {
  
-  return( 
-  <h1>404 - Page Not Found</h1>
-  )
+	return(
+		<main className={styles.container}>
+			<h1 className={styles.card}>404 - Page Not Found</h1>
+		</main>
+	)
 }
