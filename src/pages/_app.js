9d508b5989e9975e7108061f330786a1e2ea5d13
Carlos Eduardo <eduardosantoos573@gmail.com>
2024-01-11 22:10:39 -0300

    change: remove Context Changelog

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,10 @@
 import { AuthProvider } from '@/contexts/AuthContext'
-import { ChangelogProvider } from '@/contexts/ChangelogContext'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  const isIndexPage = Component.name == 'Changelog'
-  return (
-    <AuthProvider>
-    {isIndexPage ? (
-        <ChangelogProvider>
-          <Component {...pageProps} />
-        </ChangelogProvider>
-      ) : (
-        <Component {...pageProps} />
-      )}
-    </AuthProvider>
-  )
+	return (
+		<AuthProvider>
+			<Component {...pageProps} />
+		</AuthProvider>
+	)
 }
