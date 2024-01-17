c362002141cd95ca8399f53b27840d6a7e9c6e83
Carlos Eduardo <eduardosantoos573@gmail.com>
2024-01-16 23:51:09 -0300

    change: pages to register adjustament

--- src/pages/signIn/register/index.jsx
+++ src/pages/signIn/register/index.jsx
@@ -1,7 +1,18 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+import { useAuth } from '@/hooks/useAuth'
 import styles from '@/pages/signIn/register/styles.module.css'
+import Button from '@/components/Button'
 
 export default function Register(){
+	const { register, handleSubmit, formState: {errors} } = useForm()
+	const { signUp } = useAuth()
+
+	async function handleRegister(data) {
+		await signUp(data)
+	}
+
 	return(
 		<>
 			<Head>
@@ -11,15 +22,71 @@ export default function Register(){
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className="container">
-				<article>
-					<form action="">
-						<fieldset>
-							<legend>Cadastro</legend>
-							
-						</fieldset>
-					</form>
-				</article>
+			<main className={styles.container}>
+				<form onSubmit={handleSubmit(handleRegister)}>
+					<fieldset className={styles.signUp}>
+						<legend>Cadastro</legend>
+						<div className={styles.formGroup}>
+							<label htmlFor="name">Nome Completo: </label>
+							<input
+								{...register('name')} 
+								type="text" 
+								name="name" 
+								id="name" 
+							/>
+						</div>
+						<div className={styles.formGroup}>
+							<label htmlFor="email">E-mail: </label>
+							<input
+								{...register('email')} 
+								type="email" 
+								name="email" 
+								id="email" 
+							/>
+						</div>
+						<div className={styles.formGroup}>
+							<label htmlFor="password">Password: </label>
+							<input
+								{...register('password')} 
+								type="password" 
+								name="password" 
+								id="password" 
+							/>
+						</div>
+						<div className={styles.formGroup}>
+							<label htmlFor="CompanyName">Nome da Empresa: </label>
+							<input
+								{...register('CompanyName')}	 
+								type="text" 
+								name="CompanyName" 
+								id="CompanyName" 
+							/>
+						</div>
+						<div className={styles.formGroup}>
+							<label htmlFor="uf">Uf: </label>
+							<input
+								{...register('uf')} 
+								type="text" 
+								name="uf" 
+								id="uf" 
+							/>
+						</div>
+						<div className={styles.formGroup}>
+							<label htmlFor="city">Cidade: </label>
+							<input
+								{...register('city')} 
+								type="text" 
+								name="city" 
+								id="city" 
+							/>
+						</div>
+						<Button typeBtn={'submit'} text={'Salvar'} />
+						<div className={styles.link}>
+							<span>Já tem uma conta?</span>
+							<Link href='/signIn/auth/' className={styles.backButton}> Login</Link>
+						</div>
+					</fieldset>
+				</form>
 			</main>
 		</>
 	)
